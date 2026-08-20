7299af3e9e789322136f2971965b6184e66e1482
Yuvraj VIjay Verma <shivamverma99399@gmail.com>
2026-08-21 02:01:13 +0530

    deployment

--- backend/run.py
+++ backend/run.py
@@ -1,10 +1,12 @@
+import os
 import uvicorn
 from app.core.config import settings
 
 if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8000))
     uvicorn.run(
         "app.main:app",
         host="0.0.0.0",
-        port=8000,
+        port=port,
         reload=settings.APP_ENV == "development",
     )
